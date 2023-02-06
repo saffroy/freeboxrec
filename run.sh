50527f204e6912388b00900550544f8a9d1cde89
Jean-Marc Saffroy <saffroy@gmail.com>
2023-02-06 22:48:48 +0100

    run.sh: disable debug, bind to localhost

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 . venv/bin/activate
 
 LC_ALL=C.UTF-8 LANG=C.UTF-8 \
-      FLASK_DEBUG=1 \
+      FLASK_DEBUG=0 \
       FLASK_APP=app.py \
       FREEBOXREC_OUTDIR=/backup/ext/films \
-      flask run --host 0.0.0.0 --port 8082 "$@"
+      exec flask run --port 8082 "$@"
