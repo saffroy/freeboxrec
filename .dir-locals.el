4a6f9b8bb7a6175776f24f65c6744630eaf039a7
Jean-Marc Saffroy <saffroy@gmail.com>
2023-02-22 14:40:06 +0100

    Revert "compute full path to venv for emacs jedi"
    This reverts commit 581a96f4fb1f0c897e37a51c7fc974053dae8a70.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((python-mode
-  (eval . (setq-local python-shell-virtualenv-root (concat default-directory "venv")))
+  (python-shell-virtualenv-root . "~/prog/python/freeboxrec/venv")
   (python-shell-interpreter . "python3")))
+
